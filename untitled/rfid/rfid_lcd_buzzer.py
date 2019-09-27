9bf2907f31787467fc50f52e291f59985bdb6afe
Capel001 <willychang008@gmail.com>
2019-09-27 10:18:37 +0800

    update

---
+++ untitled/rfid/rfid_lcd_buzzer.py
@@ -0,0 +1,134 @@
+from tkinter import *
+from threading import Timer
+import RPi.GPIO as GPIO
+import mfrc522 as MFRC522
+from lcd_display import lcd as Lcd
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import auth
+
+class App:
+    def __init__(self,window):
+        # init firebase
+        self.cred = credentials.Certificate(
+            '/home/pi/Documents/iotcapel-firebase-adminsdk-ps4ca-4caaa40275.json')
+        firebase_admin.initialize_app(self.cred, {
+            'databaseURL': 'https://iotcapel.firebaseio.com/',
+            'databaseAuthVariableOverride': {'uid': 'XRRA8T9973hygQDy5W4JhTMGJqh2'}
+        })
+
+        # rfid init
+        self.uid = []
+        self.preUid0 = 0
+        self.preUid1 = 0
+        self.preUid2 = 0
+        self.preUid3 = 0
+
+        #buzzer frame button
+        buzzerFrame = Frame(window)
+        Button(buzzerFrame,text="Buzzer Control", padx=10, pady=10,command=self.userClickBuzzer).pack(expand=YES, fill=BOTH,padx=30,pady=10)
+        buzzerFrame.pack(expand=YES, fill=BOTH)
+        
+        #lcd frame
+        lcdFrame = Frame(window)
+        self.entryString1 = StringVar()
+        self.entryString2 = StringVar()
+        
+        #grid
+        #entry1Frame
+        
+        Label(lcdFrame,text="name").grid(row=0, column=0, sticky=W,padx=5,pady=5)
+        Entry(lcdFrame,textvariable=self.entryString1,width=16).grid(row=0, column=1, sticky=W,padx=5,pady=5)
+        self.entryString1.set("First Line")
+        
+        Label(lcdFrame,text="pwd").grid(row=1, column=0, sticky=W,padx=5, pady=5)
+        Entry(lcdFrame,textvariable=self.entryString2,width=16).grid(row=1, column=1, sticky=W,padx=5,pady=5)
+        self.entryString2.set("Second Line")
+        
+        Button(lcdFrame,text="send",command=self.userClickSend,padx=10,pady=10).grid(row=2,column=1 ,padx=5,pady=5)
+        lcdFrame.pack()
+        
+        #buzzer init
+        #self.buzzer = Buzzer(16)
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(36,GPIO.OUT)
+        self.buzzer = GPIO.PWM(36, 50)
+        
+        
+        
+        #rfid init
+        self.MIFAREReader = MFRC522.MFRC522()
+        self.rfidHandler()
+        
+        #lcd init
+        self.lcd = Lcd()
+        
+        
+    def userClickBuzzer(self):
+        print("user click")
+        self.buzzer.start(50)
+        self.buzzer.ChangeFrequency(3500)
+        t = Timer(0.1,self.closeBuzzer)
+        t.start()
+        
+        
+    
+    def closeBuzzer(self):
+        self.buzzer.stop()
+    
+    def userClickSend(self):
+        '''
+               entry1Words = self.entryString1.get();
+               entry2Words = self.entryString2.get();
+               self.lcd.display_string(entry1Words,1);
+               self.lcd.display_string(entry2Words,2);
+               '''
+        try:
+            entryEmail = self.entryString1.get()
+            user = auth.get_user_by_email(entryEmail)
+            self.lcd.display_string(user.uid, 1)
+            self.lcd.display_string('success', 2)
+            print(user.uid)
+        except:
+            self.lcd.display_string('incorrect', 1)
+            self.lcd.display_string('who are you?', 2)
+
+    def rfidHandler(self):
+        # scan for cards
+        (status, tagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
+
+        if status == self.MIFAREReader.MI_OK:
+            (status, self.uid) = self.MIFAREReader.MFRC522_Anticoll()
+            if status == self.MIFAREReader.MI_OK:
+                uid0 = self.uid[0]
+                uid1 = self.uid[1]
+                uid2 = self.uid[2]
+                uid3 = self.uid[3]
+
+            if uid0 != self.preUid0 or uid1 != self.preUid1 or uid2 != self.preUid2 or uid3 != self.preUid3:
+                self.preUid0 = uid0
+                self.preUid1 = uid1
+                self.preUid2 = uid2
+                self.preUid3 = uid3
+                uidString = str(uid0) + str(uid1) + str(uid2) + str(uid3)
+                # lcd
+                self.lcd.display_string(uidString, 1)
+
+                # buzzer
+                print(uidString)
+                self.buzzer.start(50)
+                Timer(0.1, self.closeBuzzer).start()
+
+        Timer(0.1, self.rfidHandler).start()
+        
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("RFID_LCD_BUZZER")
+    root.option_add("*font",("verdana", 18))
+    root.option_add("*background", "#cccccc")
+    root.option_add("*forground","#888888")
+    app = App(root)
+    root.mainloop()
